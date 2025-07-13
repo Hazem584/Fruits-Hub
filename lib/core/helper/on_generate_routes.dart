ea81b16c821b121375ebe755ac5b128693036bcf
Hazem Mohamed <hazemhkcyy@gmail.com>
2025-07-13 19:11:46 +0300

    splash view ui and logic done

---
+++ lib/core/helper/on_generate_routes.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:fruits_e_commerce/features/onbording/ui/screen/on_bording_view.dart';
+
+import '../../features/splash/ui/screen/splash_view.dart';
+
+Route<dynamic> onGenerateRoutes(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (_) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnBoardingView());
+    default:
+      return MaterialPageRoute(builder: (_) => const SplashView());
+  }
+}
---
+++ lib/features/splash/ui/widgets/splash_view_body.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_e_commerce/core/utils/app_images.dart';
+import 'package:fruits_e_commerce/features/onbording/ui/screen/on_bording_view.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SvgPicture.asset(Assets.imagesPlant)],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
+      ],
+    );
+  }
+  
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
+}
