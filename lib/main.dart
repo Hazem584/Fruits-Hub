54b5842bac8b96c695c3df0097c5a661f6f96f78
Hazem Mohamed <hazemhkcyy@gmail.com>
2025-07-20 14:02:09 +0300

    Integrate Firebase: Add Firebase configuration files, initialize Firebase in main, and update dependencies for Firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -5,11 +6,13 @@ import 'package:fruits_e_commerce/core/helper/on_generate_routes.dart';
 import 'package:fruits_e_commerce/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_e_commerce/core/theming/app_colors.dart';
 import 'package:fruits_e_commerce/features/splash/ui/screen/splash_view.dart';
+import 'package:fruits_e_commerce/firebase_options.dart';
 import 'package:fruits_e_commerce/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtil.ensureScreenSize();
   runApp(const FruitHub());
 }
